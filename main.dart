504f2d33b03e1290dd18ff4a9f4ade96e8f9955e
kundan1094 <66164495+kundan1094@users.noreply.github.com>
2021-02-07 10:40:00 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,19 +1,28 @@
+import 'package:a_commerce/screens/landing_page.dart';
 import 'package:flutter/material.dart';
-import 'hme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() =>runApp(MyApp());
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:'flutter app',
       theme: ThemeData(
-        primaryColor:  Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        accentColor: Color(0xFFFF1E00)
       ),
-      home:  hme(),
-
+      home: LandingPage(),
     );
   }
 }
+
+
+
+
